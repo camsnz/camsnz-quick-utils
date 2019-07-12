e48a68d283d1d5f8845e9e2ba09ba65fb3bafa8b
Cam Scott <cameronscott@gmail.com>
2019-07-12 14:48:14 +1000

    object-query: add queryFilter(), with _conjunction {union,intersection}

--- src/object-query/object-query.test.ts
+++ src/object-query/object-query.test.ts
@@ -1,26 +1,15 @@
-import {match, queryMatch} from "./object-query"; // matchQueryParams
+import { match, queryMatch, queryFilter } from "./object-query"; // matchQueryParams
 
 const mockObj = {
-  objs: [
-    {t: 1},
-    {t: 2}
-  ],
-  strs: [
-    "a",
-    "b",
-    "c",
-  ],
+  objs: [{ t: 1 }, { t: 2 }, { t: 3 }],
+  strs: ["a", "b", "c"],
   nest1: [
     {
       nest2: {
-        nest3: [
-          "x",
-          "y",
-          "z"
-        ],
+        nest3: ["x", "y", "z"],
         message: "well done",
         obj: {
-          message: "well done2",
+          message: "well done2"
         }
       }
     }
@@ -28,94 +17,112 @@ const mockObj = {
 
   obj1: {
     obj2: {
-      objs: [
-        {t: 1},
-        {t: 2}
-      ],
+      objs: [{ t: 1 }, { t: 2 }],
       message: "sample"
     }
-  }
+  },
+
+  complexes: [
+    { x: { y: [{ z: 1 }, { z: 2 }] } },
+    { x: { y: [{ z: 3 }, { z: 4 }] } },
+    { x: { y: [{ z: 5 }, { z: 6 }] } },
+    { x: { y: [{ z: 7 }, { z: 8 }] } }
+  ]
 };
 
-const mockArray =[
-  mockObj,
-  {nest1: [{ message: "not nest2" }]},
-  {nest1: [{ nest2: { message: "not well done" }}]},
-];
-
+const mockArray = [mockObj, { nest1: [{ message: "not nest2" }] }, { nest1: [{ nest2: { message: "not well done" } }] }];
 
 describe("match", () => {
-  it("should match nested string", () => {
-    // for: { "obj1.obj2.message": "sample" }
-    expect(
-      match(mockObj, "obj1.obj2.message", "sample")
-    ).toBe(true);
-    expect(
-      match(mockObj, "obj1.obj2.message", "nope")
-    ).toBe(false);
-  });
+  describe("matching", () => {
+    it("should match nested string", () => {
+      // for: { "obj1.obj2.message": "sample" }
+      expect(match(mockObj, "obj1.obj2.message", "sample")).toBe(true);
+      expect(match(mockObj, "obj1.obj2.message", "nope")).toBe(false);
+    });
 
-  it("should match array value", () => {
-    // for: { "obj1.obj2.message": "sample" }
-    expect(
-      match(mockObj, "strs", "b")
-    ).toBe(true);
-  });
+    it("should match array value", () => {
+      // for: { "obj1.obj2.message": "sample" }
+      expect(match(mockObj, "strs", "b")).toBe(true);
+    });
 
-  it("should match nested array values", () => {
-    expect(
-      match(mockObj, "nest1.nest2.message", "well done")
-    ).toBe(true);
+    it("should match nested array values", () => {
+      expect(match(mockObj, "nest1.nest2.message", "well done")).toBe(true);
 
-    expect(
-      match(mockObj, "nest1.nest2.obj.message", "well done2")
-    ).toBe(true);
+      expect(match(mockObj, "nest1.nest2.obj.message", "well done2")).toBe(true);
 
-    expect(
-      match(mockObj, "nest1.nest2.nest3", "y")
-    ).toBe(true);
-  });
+      expect(match(mockObj, "nest1.nest2.nest3", "y")).toBe(true);
+    });
 
-  it("should match mockArray nested array values", () => {
-    expect(
-      match(mockArray, "nest1.nest2.message", "extra well done")
-    ).toBe(false);
+    it("should match mockArray nested array values", () => {
+      expect(match(mockArray, "nest1.nest2.message", "extra well done")).toBe(false);
 
-    const newMockArray = [
-      ...mockArray,
-      {nest1: [{ nest2: { message: "extra well done" }}]},
-    ];
-    expect(
-      match(newMockArray, "nest1.nest2.message", "extra well done")
-    ).toBe(true);
-
+      const newMockArray = [...mockArray, { nest1: [{ nest2: { message: "extra well done" } }] }];
+      expect(match(newMockArray, "nest1.nest2.message", "extra well done")).toBe(true);
+    });
   });
 });
 
-
-// options: __strict=true, __conjugate=or,and,xor
+// future options: __strict=true, __conjugate=or,and,xor
 describe("queryMatch", () => {
-
-  const mockObj = {
+  const qmMockObj = {
     persons: [
-      { name: "mike", id: 5, limbs: ["LH","LF"] },
-      { name: "sash", id: 10, limbs: ["LH","LF","RF"] },
-    ],
+      { name: "mike", id: 5, limbs: ["LH", "LF"] },
+      { name: "sash", id: 10, limbs: ["LH", "LF", "RF"] },
+      { name: "brit", id: 15, limbs: ["LH", "LF"], pets: [{ type: "dog" }] }
+    ]
   };
 
-  it("should match total object for RF", () => {
-    expect(
-      queryMatch({ "persons.limbs": "RF" })(mockObj)
-    ).toBe(true);
+  describe("matching", () => {
+    it("should match total object for RF", () => {
+      expect(queryMatch({ "persons.limbs": "RF" })(qmMockObj)).toBe(true);
+    });
+
+    it("should match person with RF", () => {
+      expect(queryMatch({ limbs: "RF" })(qmMockObj.persons[0])).toBe(false);
+
+      expect(queryMatch({ limbs: "RF" })(qmMockObj.persons[1])).toBe(true);
+    });
+
+    it("should match person with RF or a dog", () => {
+      expect(qmMockObj.persons.filter(queryMatch({ limbs: "RF", "pets.type": "dog" }))).toEqual([qmMockObj.persons[1], qmMockObj.persons[2]]);
+    });
   });
 
-  it("should match person with RF", () => {
-    expect(
-      queryMatch({ "limbs": "RF" })(mockObj.persons[0])
-    ).toBe(false);
+  describe("multiple values in a string", () => {
+    it("should match for comma separated values", () => {
+      expect(match(mockObj, "strs", "b,c")).toBe(true);
 
-    expect(
-      queryMatch({ "limbs": "RF" })(mockObj.persons[1])
-    ).toBe(true);
+      expect(mockObj.complexes.filter(queryMatch({ "x.y.z": "3,7" }))).toEqual([
+        { x: { y: [{ z: 3 }, { z: 4 }] } },
+        { x: { y: [{ z: 7 }, { z: 8 }] } }
+      ]);
+    });
+  });
+});
+
+describe("queryFilter", function() {
+  const qmMockObj = {
+    persons: [
+      { name: "mike", id: 5, limbs: ["LH", "LF"] },
+      { name: "sash", id: 10, limbs: ["LH", "LF", "RF"] },
+      { name: "brit", id: 15, limbs: ["LH", "LF"], pets: [{ type: "dog" }] }
+    ]
+  };
+
+  describe("matching conjunctions", () => {
+    it("should match union", () => {
+      expect(queryFilter(qmMockObj.persons, {
+        _conjunction: "union",
+        limbs: "RF",
+        id: "5"
+      })).toEqual([qmMockObj.persons[0], qmMockObj.persons[1]]);
+    });
+    it("should match intersection", () => {
+      expect(queryFilter(qmMockObj.persons, {
+        _conjunction: "intersection",
+        limbs: "LH",
+        id: "5"
+      })).toEqual([qmMockObj.persons[0]]);
+    });
   });
 });
--- src/object-query/object-query.ts
+++ src/object-query/object-query.ts
@@ -1,25 +1,83 @@
 import _ from "lodash";
 
-export const queryMatch = (queryParams:object) => (obj:any) =>
-  !!_.toPairs(queryParams).map(
-    (([key, value]) => match(obj, key, value))
-  ).find((r:any) => r===true);
+const toYesOrNo = (tf: any) => (tf === true || "yes" == _.toLower(tf) ? "yes" : "no");
 
+const CONJUNCTION = {
+  INTERSECTION: "intersection",
+  UNION: "union"
+};
 
-export const match = (obj:any, qk:string, qv:any):boolean => {
+export const queryMatch = (queryParams: object) => (obj: any) =>
+  !!_.toPairs(queryParams)
+    .map(([key, value]) => match(obj, key, value))
+    .find((r: any) => r === true);
+
+const queryFilterIntersection = (objects: Array<any>, queryParams: any) => {
+  let filtered = objects;
+  _.toPairs(queryParams).forEach(([key, value]) => {
+    // match(obj, key, value)
+    filtered = filtered.filter((obj) => match(obj, key, value));
+  });
+  return filtered;
+};
+
+const queryFilterUnion = (objects: Array<any>, queryParams: any) =>
+  objects.filter((obj) =>
+    _.toPairs(queryParams).map(
+      ([key, value]) => match(obj, key, value)
+    ).find(r => r === true)
+  );
+
+export const queryFilter = (objects: Array<any>, queryParams: any) => {
+  if (_.keys(queryParams).length == 0) {
+    return objects;
+  }
+
+  const { _conjunction } = queryParams;
+  delete queryParams._conjunction;
+
+  if (_conjunction === CONJUNCTION.INTERSECTION) {
+    return queryFilterIntersection(objects, queryParams);
+  }
+  if (_conjunction === CONJUNCTION.UNION) {
+    return queryFilterUnion(objects, queryParams);
+  }
+
+  // default
+  return queryFilterIntersection(objects, queryParams);
+};
+
+export const match = (obj: any, qk: string, qv: any): boolean => {
+  if (typeof qv === "string") {
+    // ignore empty query value
+    qv = qv.trim();
+    if (qv == "") {
+      return false;
+    }
+
+    // split-recurse multi-value eg: "1,4,6"
+    if (qv.includes(",")) {
+      return (
+        qv
+          .split(",")
+          .map((splitQv: string) => match(obj, qk, splitQv))
+          .find((r: boolean) => r === true) === true
+      );
+    }
+  }
 
   // recurse arrays
-  if(_.isArray(obj)) {
+  if (_.isArray(obj)) {
     return matchArray(obj, qk, qv);
   }
 
   // recurse arrays anywhere in qk path
   const qks = qk.split(".");
-  for(let i=0; i<qks.length; i++) {
-    const subQk = qks.slice(0,i).join(".");
+  for (let i = 0; i < qks.length; i++) {
+    const subQk = qks.slice(0, i).join(".");
     const subQkObj = _.get(obj, subQk);
-    if(_.isArray(subQkObj)) {
-      return matchArray(subQkObj, qks.slice(i,qks.length).join("."), qv);
+    if (_.isArray(subQkObj)) {
+      return matchArray(subQkObj, qks.slice(i, qks.length).join("."), qv);
       // const results = subQkObj.map(
       //   (val:any, idx:number) => match(subQkObj[idx], qks.slice(i,qks.length).join("."), qv)
       // );
@@ -29,22 +87,21 @@ export const match = (obj:any, qk:string, qv:any):boolean => {
 
   const v = _.get(obj, qk);
 
-  if(_.isArray(v)) {
+  if (_.isArray(v)) {
     // @ts-ignore
-    return !!v.find((val:any) => _.toLower(val) == _.toLower(qv));
+    return !!v.find((val: any) => _.toLower(val) == _.toLower(qv));
   }
 
-  if(typeof v === "object") {
+  if (typeof v === "object") {
     return false;
   }
 
-  return _.toLower(v) == _.toLower(qv);
+  const lv = _.toLower(v);
+  return lv == _.toLower(qv) || lv == toYesOrNo(qv);
 };
 
-const matchArray = (array:any, qk:string, qv:any) => {
-  const results = array.map(
-    (val:any, idx:number) => match(array[idx], qk, qv)
-  );
+const matchArray = (array: any, qk: string, qv: any) => {
+  const results = array.map((val: any, idx: number) => match(array[idx], qk, qv));
   // return _.uniq(results).length === 2 && results[0];
-  return results.find((r:any) => r===true)===true;
+  return results.find((r: any) => r === true) === true;
 };
